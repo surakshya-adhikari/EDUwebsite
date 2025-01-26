3d8e5f2fa9851e3bad29e75b78addf22f50cf666
surakshya-adhikar <surakshyaadhikari09@gmail.com>
2025-01-26 19:23:28 +0545

    Integrate Contact component into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
 import Collegephoto from './Components/Collegephoto/Collegephoto'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
+
 
 const App = () => {
   return (
@@ -17,7 +20,11 @@ const App = () => {
             <About/> 
             <Title subTitle='Gallery' title='college photo'/>
             <Collegephoto/>
-            <Title subTitle='TESTIMONALS' title='What student says?'/>
+            <Title subTitle='Testimonials' title='What students says'/>
+            <Testimonials/>
+            <Title subTitle='Contact Us' title='Get in touch'/>
+            <Contact/>
+            
         </div>
         
     </div>
